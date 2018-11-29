d8af7ff4c1a0a56641dbf2faeafb0b02e90b868f
loulou <yourdj.france@gmail.com>
2018-11-29 16:03:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/haroldyourdj/app11/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.haroldyourdj.app11;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+}
